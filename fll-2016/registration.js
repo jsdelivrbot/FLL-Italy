7d4ddabb3b8cbd5593a76cb2a665abae462751f3
Alessandro Pezzè <igougi.ui@gmail.com>
2016-07-01 17:20:12 +0200

    reg

---
+++ fll-2016/registration.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var table = '<table><thead><tr><th>Fase</th><th>Iscritte</th></tr></thead>#</table>'
+	var request = new XMLHttpRequest();
+  request.open('GET', 'https://rawgit.com/Naramsim/FLL-Italy/master/fll-2016/fllTeams.json', true);
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var data = JSON.parse(this.response);
+      var regions = {	'NE': ['Nord Est', 0], 
+                      'NO': ['Nord Ovest', 0],
+                      'C' : ['Centro', 0],
+                      'S' : ['Sud', 0],
+                      'IC': ['Isole e Calabria', 0]};
+      Object.keys(data).forEach(function(key) {
+      	regions[data[key].region][1] += 1;
+      })
+      var intermidiate = '';
+      Object.keys(regions).forEach(function(key){
+      	intermidiate += '<tr><td>' + regions[key][0] + '</td><td>' + regions[key][1] + '</td></tr>'
+      })
+      table = table.replace('#', intermidiate)
+      document.getElementById('registration').insertAdjacentHTML('beforeend', table);
+    }
+  };
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+  };
+
+  request.send();
+})
