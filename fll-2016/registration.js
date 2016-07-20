7b6a16b7cb8c54f2cf4d017ab70efffc766ecc2b
Alessandro Pezzè <igougi.ui@gmail.com>
2016-07-20 11:32:47 +0200

    reg

--- fll-2016/registration.js
+++ fll-2016/registration.js
@@ -1,34 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var baseURL = 'http://fll-italia.it/fll/2016/teams/#';
-	 var table = "<font size='+1'><b>Squadre Iscritte</b></font>" + 
+    var baseURL = 'http://fll-italia.it/fll/2016/teams/#';
+    var table = "<font size='+1'><b>Squadre Iscritte</b></font>" + 
     '<table><thead><tr><th>Fase</th><th>Iscritte</th></tr></thead>#</table>';
-	var request = new XMLHttpRequest();
-  request.open('GET', 'https://rawgit.com/Naramsim/FLL-Italy/master/fll-2016/fllTeams.json', true);
+    var currentPath = window.location.pathname;
+    var currentGame = /^\/(\w+)\/2016/.exec(currentPath)
+    var regionsFLL = {  'nord-est': 0, 
+                        'nord-ovest': 0,
+                        'centro': 0,
+                        'sud': 0,
+                        'isole-e-calabria': 0};
+    var regionsJr = {   'rovereto': 0,
+                        'genova': 0,
+                        'pistoia': 0,
+                        'catania': 0,
+                        'brescia': 0,
+                        'settimo-torinese': 0,
+                        'pachino': 0};
 
-  request.onload = function() {
-    if (this.status >= 200 && this.status < 400) {
-      // Success!
-      var data = JSON.parse(this.response);
-      var regions = {	'NE': ['Nord Est', 0], 
-                      'NO': ['Nord Ovest', 0],
-                      'C' : ['Centro', 0],
-                      'S' : ['Sud', 0],
-                      'IC': ['Isole e Calabria', 0]};
-      Object.keys(data).forEach(function(key) {
-      	regions[data[key].region][1] += 1;
-      })
-      var intermediate = '';
-      Object.keys(regions).forEach(function(key){
-      	intermediate += '<tr><td><a href="' + baseURL + regions[key][0].replace(/\s/g, '-').toLowerCase() + '">' + regions[key][0] + '</a></td><td>' + regions[key][1] + '</td></tr>'
-      })
-      table = table.replace('#', intermediate)
-      document.getElementById('registration').insertAdjacentHTML('beforeend', table);
+    if (currentGame[1] === 'fll') {
+        build('fll', regionsFLL)
+    } else if (currentGame[1] === 'junior') {
+        build('fllJr', regionsJr)
     }
-  };
-
-  request.onerror = function() {
-    // There was a connection error of some sort
-  };
-
-  request.send();
 })
+
+function build(game, regions) {
+    var request = new XMLHttpRequest();
+
+    request.open('GET', 'https://rawgit.com/Naramsim/FLL-Italy/master/fll-2016/' + game + 'Teams.json', true);
+    request.onload = function() {
+        if (this.status >= 200 && this.status < 400) {
+            var data = JSON.parse(this.response);
+            var intermediate = '';
+
+            Object.keys(data).forEach(function(key) {
+                regions[data[key].region] += 1;
+            })
+            Object.keys(regions).forEach(function(key){
+                intermediate += '<tr><td><a href="' + baseURL + key + '">' + key + '</a></td><td>' + regions[key] + '</td></tr>'
+            })
+            
+            table = table.replace('#', intermediate)
+            document.getElementById('registration').insertAdjacentHTML('beforeend', table);
+        }
+    };
+    request.onerror = function() {};
+    request.send();
+}
+
+function up(str) {
+    return string.charAt(0).toUpperCase() + string.slice(1).replace('-', ' ');
+}
