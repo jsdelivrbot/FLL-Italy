c28886d51659cd6bef1b1a05174b758748d990f3
Alessandro Pezzè <igougi.ui@gmail.com>
2016-10-22 11:15:53 +0200

    update

--- fll-2016/teams.js
+++ fll-2016/teams.js
@@ -12,11 +12,13 @@ var origin = {
 
 document.addEventListener('DOMContentLoaded', function() {
     var baseUrlFLL = "http://fll-italia.it/fll/2016/";
-    var baseUrlJr = "http://fll-italia.it/junior/2016/";
+    var baseUrlFLLJr = "http://fll-italia.it/junior/2016/";
     //var links = ["nord-ovest", "nord-est", "centro", "sud", "isole-e-calabria"];
     var hash = window.location.hash;
     var teamFLLRe = /^#FLL(\d+)$/;
     var teamJrRe = /^#FLLJR(\d+)$/;
+    var teamAllFLLRe = /^#FLL$/;
+    var teamAllFLLJrRe = /^#FLLJr$/;
     var regionFLLRe = /^#(?:nord-ovest|nord-est|centro|sud|isole-e-calabria)$/;
     var regionJrRe = /^#(?:rovereto|genova|pistoia|catania|brescia|settimo-torinese|pachino)$/;
 
@@ -24,19 +26,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
     var teamFLL = teamFLLRe.exec(hash);
     var teamJr = teamJrRe.exec(hash);
+
     var regionFLL = regionFLLRe.exec(hash);
     var regionJr = regionJrRe.exec(hash);
 
+    var teamFLLAll = teamAllFLLRe.exec(hash);
+    var teamFLLJrAll = teamAllFLLJrRe.exec(hash);
+
     if (regionFLL || teamFLL) {
-        build('fllTeams', regionFLL, teamFLL, baseUrlFLL, 'FLL')
+        build('fllTeams', regionFLL, teamFLL, baseUrlFLL, 'FLL');
     } else if (regionJr || teamJr) {
-        build('fllJrTeams', regionJr, teamJr, baseUrlJr, 'FLLJr');
-    } 
+        build('fllJrTeams', regionJr, teamJr, baseUrlFLLJr, 'FLLJr');
+    } else if (teamFLLAll) {
+    	build("", false, false, baseUrlFLL, 'FLL');
+    } else if (teamFLLJrAll) {
+    	build("", false, false, baseUrlFLLJr, 'FLLJr');
+    }
 })
 
 function build(url, region, team, baseUrl, teamUrl) {
     fetchJSONFile('https://rawgit.com/Naramsim/FLL/master/fll-2016/' + url + '.json', function(data){
-        console.log(data);
+        // console.log(data);
         var semiFinalLink = "";
         var intermediate = "";
         var teamLink = "";
@@ -49,7 +59,7 @@ function build(url, region, team, baseUrl, teamUrl) {
         if(team) {
 
         } else if (region) {
-            var over = '<h3>Squadre iscritte alla fase #</h3><div>Le squadre provengono da:@</div></br>'
+            var over = '<h3>Squadre iscritte alla fase #</h3><div>Le squadre provengono da: @</div></br>'
 
             if (url === 'fllTeams') {
                 over = over.replace('#', region).replace('@', origin[region])
@@ -69,7 +79,7 @@ function build(url, region, team, baseUrl, teamUrl) {
                                     data[key].name + 
                                     // "</a>" +
                                     "</td>";
-                    intermediate += "<td>" + data[key].city + "</td>";
+                    intermediate += "<td><a href=http://maps.google.com/?q=" + data[key].city + ">" + data[key].city + "</a></td>";
                     intermediate += "<td>" +
                                     // "<a href=" + semiFinalLink + ">" +
                                     data[key].region + 
@@ -87,7 +97,7 @@ function build(url, region, team, baseUrl, teamUrl) {
                     intermediate += '<tr>';
                     intermediate += "<td>" + teamUrl + key + "</td>";
                     intermediate += "<td><a href=" + teamLink + ">" + data[key].name + "</a></td>";
-                    intermediate += "<td>" + data[key].city + "</td>";
+                    intermediate += "<td><a href=http://maps.google.com/?q="+ data[key].city + ">" + data[key].city + "</a></td>";
                     intermediate += "<td><a href=" + semiFinalLink + ">" + data[key].region + "</a></td></tr>";
                 }
             }
