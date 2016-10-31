edb9c61c06a9e7a79502dd6031aa2b7eb6825ae2
Alessandro Pezzè <igougi.ui@gmail.com>
2016-10-31 10:21:19 +0100

    test

--- fll-2016/teams.js
+++ fll-2016/teams.js
@@ -81,6 +81,8 @@ function build(url, region, team, baseUrl) {
             }
 
             data.forEach(function(row) {
+                console.log(row['iscrizione a qualificazione regionale'])
+                console.log(region)
                 if (region == convertCityToRegion(row['iscrizione a qualificazione regionale'], url)) {
                     semiFinalLink = baseUrl + 'semi-final/#' + convertCityToRegion(row['iscrizione a qualificazione regionale'], url);
                     teamLink = baseUrl + 'teams/#' + row['nr. Iscrizione'];
